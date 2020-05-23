5a148d5f68087adda0e4d6383db121729aaf7c2d
NityaMahata <nitya040590@gmail.com>
2020-05-23 16:08:32 +0530

    Add files via upload

---
+++ storage/framework/views/bd219f695aea99f72f3d968abe04065fdd56d5b8.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>The Yummi Pizza</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="css/app.css" rel="stylesheet" type="text/css"> </link>
+        
+    </head>
+    <body>
+       <div id="menu"> </div>
+       
+       <script type="text/javascript" src="js/app.js" > </script>
+    </body>
+</html>
+<?php /**PATH N:\workspace\htdocs\laravel\lara-react\resources\views/home.blade.php ENDPATH**/ ?>
